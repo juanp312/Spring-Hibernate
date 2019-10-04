046f25f12b83dede5a0ed849959b4684200cbe43
juanp312 <juanechavarria315@gmail.com>
2019-10-04 17:11:47 -0500

    JDBC Querys

---
+++ src/main/java/com/jpa/hibernate/studying/demo/Application.java
@@ -0,0 +1,33 @@
+package com.jpa.hibernate.studying.demo;
+
+import Entity.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	PersonJdbcDao dao;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("All users -> {}", dao.findall());
+		logger.info("User 12001 -> {}", dao.findById(12001));
+		logger.info("Deleting 13001 -> {}", dao.deleteById(13001));
+		logger.info("Inserting 19001 -> {}", dao.insert(new Person(19001, "Ramon", "New York", new Date())));
+		logger.info("Update 14001 -> {}", dao.update(new Person(14001, "Maria", "Madrid", new Date())));
+	}
+}
